977d0e5ceaedc22341d74c5829606a7bf34aa625
zhiyimen <zhiyimen@bu.edu>
2024-04-09 22:24:12 -0400

    complete practice 3

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,23 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+const myName = "Kelly";
+const myAge = "23";
+console.log(myAge);
+
+const toggledParagraph = document.querySelector(".show");
+console.log(toggledParagraph);
+
+function toggleParagraph() {
+    toggledParagraph.classList.toggle("hide");
+    toggledParagraph.classList.toggle("show");
+    console.log("My toggle function");
+    console.log(toggledParagraph);
+}
+
+
+const toggleButton = document.querySelector("#toggle-control");
+console.log(toggleButton);
+
+toggleButton.addEventListener("click",toggleParagraph);
